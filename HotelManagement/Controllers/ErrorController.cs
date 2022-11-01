41bdf700882b6af3988028d86e145ed7fdbbd529
tundevtrai123 <giatunn123@gmail.com>
2022-11-01 11:53:22 +0700

    layout template for account and error

---
+++ HotelManagement/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HotelManagement.Controllers
+{
+    [Route("Error")]
+    public class ErrorController : Controller
+    {
+        // GET: /<controller>/
+        [Route("404")]
+        public IActionResult Error404()
+        {
+            return View("Error404");
+        }
+
+        [Route("500")]
+        public IActionResult Error500()
+        {
+            return View("Error500");
+        }
+    }
+}
